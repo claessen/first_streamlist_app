72305d38864eb35bafc67c41873875a542f7aa3d
claessen <paul@claessen.org>
2022-10-15 00:27:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -20,3 +24,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table
 streamlit.dataframe(fruits_to_show)
+
+streamlit.text(fruityvice_response)
